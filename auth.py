276962b8df2848f18261ba761c30813b20ad5c9a
el7futuro <el7futuro@gmail.com>
2022-10-02 11:49:53 +0500

    1.1

---
+++ auth.py
@@ -0,0 +1,112 @@
+import jwt
+from flask import request, abort
+from flask_restx import Namespace, Resource
+import calendar
+import datetime
+from config import Config
+from models import User
+from setup_db import db
+
+secret = Config.SECRET_HERE
+algo = 'HS256'
+
+auth_ns = Namespace('auth')
+
+def auth_required(func):
+    def wrapper(*args, **kwargs):
+        if 'Authorization' not in request.headers:
+            abort(401)
+
+        data = request.headers['Authorization']
+        token = data.split("Bearer ")[-1]
+        try:
+            jwt.decode(token, secret, algorithms=[algo])
+        except Exception as e:
+            print("JWT Decode Exception", e)
+            abort(401)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+
+@auth_ns.route('/')
+class AuthView(Resource):
+    def post(self):
+        req_json = request.json
+        username = req_json.get("username", None)
+        password = req_json.get("password", None)
+        if None in [username, password]:
+            abort(400)
+
+        user = db.session.query(User).filter(User.username == username).first()
+
+        if user is None:
+            return {"error": "Неверные учётные данные"}, 401
+
+        password_hash = User.get_hash(self)
+
+        if password_hash != user.password:
+            return {"error": "Неверные учётные данные"}, 401
+
+        data = {
+            "username": user.username,
+            "role": user.role
+        }
+        min30 = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        data["exp"] = calendar.timegm(min30.timetuple())
+        access_token = jwt.encode(data, secret, algorithm=algo)
+        days130 = datetime.datetime.utcnow() + datetime.timedelta(days=130)
+        data["exp"] = calendar.timegm(days130.timetuple())
+        refresh_token = jwt.encode(data, secret, algorithm=algo)
+        tokens = {"access_token": access_token, "refresh_token": refresh_token}
+
+        return tokens, 201
+
+    def put(self):
+        req_json = request.json
+        refresh_token = req_json.get("refresh_token")
+        if refresh_token is None:
+            abort(400)
+
+        try:
+            data = jwt.decode(jwt=refresh_token, key=secret, algorithms=[algo])
+        except Exception as e:
+            abort(400)
+
+        username = data.get("username")
+
+        user = db.session.query(User).filter(User.username == username).first()
+
+        data = {
+            "username": user.username,
+            "role": user.role
+        }
+        min30 = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        data["exp"] = calendar.timegm(min30.timetuple())
+        access_token = jwt.encode(data, secret, algorithm=algo)
+        days130 = datetime.datetime.utcnow() + datetime.timedelta(days=130)
+        data["exp"] = calendar.timegm(days130.timetuple())
+        refresh_token = jwt.encode(data, secret, algorithm=algo)
+        tokens = {"access_token": access_token, "refresh_token": refresh_token}
+
+        return tokens, 201
+
+def admin_required(func):
+        def wrapper(*args, **kwargs):
+            if 'Authorization' not in request.headers:
+                abort(401)
+
+            data = request.headers['Authorization']
+            token = data.split("Bearer ")[-1]
+            try:
+                user = jwt.decode(token, secret, algorithms=[algo])
+                role = user.get("role")
+                if role != "admin":
+                    abort(400)
+            except Exception as e:
+                print("JWT Decode Exception", e)
+                abort(401)
+            return func(*args, **kwargs)
+
+        return wrapper
---
+++ views/users.py
@@ -0,0 +1,52 @@
+
+
+from flask import request
+from flask_restx import Resource, Namespace
+
+from auth import auth_required, admin_required
+from models import User, UserSchema
+from setup_db import db
+
+
+
+user_ns = Namespace('users')
+
+@user_ns.route('/')
+class UsersView(Resource):
+    def get(self):
+
+        all_users = db.session.query(User).all()
+        res = UserSchema(many=True).dump(all_users)
+        return res, 200
+
+    def post(self):
+        req_json = request.json
+        # res = User(**req_json)
+        print(type(req_json))
+        req_json['password'] = User.get_hash(req_json['password'])
+        db.session.add(req_json)
+        db.session.commit()
+        return "", 201, {"location": f"/users/{req_json.id}"}
+
+
+
+@user_ns.route('/<int:uid>')
+class UserView(Resource):
+    @admin_required
+    def delete(self, uid):
+
+        user = User.query.get(uid)
+        db.session.delete(user)
+        db.session.commit()
+        return "", 204
+
+    @admin_required
+    def put(self, uid):
+        user = db.session.query(User).get(uid)
+        req_json = request.json
+        user.username = req_json.get("username")
+        user.password = req_json.get("password")
+        user.role = req_json.get("role")
+        db.session.add(user)
+        db.session.commit()
+        return "", 204
